b983dd6a431b6c4a2576a37a2750361a79f24cd5
Sania <57236398+theSaniaSalman@users.noreply.github.com>
2020-03-17 03:25:10 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+
+
+alert("Welcome to our site!");
+var name=prompt("What's your name?");
+var color=prompt("What's your favourite color?");
+document.write("Hello "+ name +", your favourite color is "+ color);
+console.log("Program Complete");
